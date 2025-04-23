1219de8a37b1f69f40270f7f2f15f597f5591eb1
Jessica Dupont <jessica.dupont1990@gmail.com>
2025-04-23 16:12:46 +0200

    chap 12 - ex 2 + 3

--- __manifest__.py
+++ __manifest__.py
@@ -19,6 +19,7 @@
         'views/property_tag_views.xml',
         'views/property_views.xml',
         'views/property_offer_views.xml',
+        'views/res_users_views.xml',
         'views/_menus.xml',#last in list
     ],
     'installable' : True,
